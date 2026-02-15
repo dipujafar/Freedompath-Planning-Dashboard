b684028dd86a69cbd48bec0e8c2a70e8e0c9e733
dipujafar <dipujafarmr@gmail.com>
2026-02-15 18:32:08 +0600

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "nazmulhasan.s3.us-east-1.amazonaws.com",
-            },
-            {
-                protocol: "https",
-                hostname: "*.amazonaws.com",
-            },
-            {
-                protocol: "http",
-                hostname: "103.186.20.117",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nazmulhasan.s3.us-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "freedom-path-planning-storage.sfo3.digitaloceanspaces.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+      },
+      {
+        protocol: "http",
+        hostname: "103.186.20.117",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
